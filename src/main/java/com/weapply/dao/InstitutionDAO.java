1af1d119fbae32eeff3077948c074b2f42e993ca
thabiso-01 <thabisoseriuos@gmail.com>
2026-04-01 15:38:24 +0200

    Create InstitutionDAO.java file

---
+++ src/main/java/com/weapply/dao/InstitutionDAO.java
@@ -0,0 +1,31 @@
+// DAO layer for managing Institution data
+
+package com.weapply.dao;
+
+import com.weapply.model.Institution;
+import java.util.List;
+
+public class InstitutionDAO {
+
+    public void addInstitution(Institution institution) {
+        // Logic to add institution
+    }
+
+    public void updateInstitution(Institution institution) {
+        // Logic to update institution
+    }
+
+    public void deleteInstitution(Long id) {
+        // Logic to delete institution by id
+    }
+
+    public Institution getInstitution(Long id) {
+        // Logic to get institution by id
+        return null;
+    }
+
+    public List<Institution> getAllInstitutions() {
+        // Logic to get all institutions
+        return null;
+    }
+}
